9b4c08db675d88ef84502f4ea6fd9063410e2223
SafonovDmitriy <49404206+SafonovDmitriy@users.noreply.github.com>
2021-01-04 12:54:21 +0200

    fix

--- src/DataBase/API/API.js
+++ src/DataBase/API/API.js
@@ -1,21 +1,8 @@
 import Server from "./../dataBase";
 
 export const API = {
-  getData: async () => {
-    const response = await Server.get();
-    return response;
-  },
-
-  getMainInfo: async () => {
-    const response = await Server.get("mainInfo");
-    return response;
-  },
-  getWorkExperience: async () => {
-    const response = await Server.get("workExperience");
-    return response;
-  },
-  getEducation: async () => {
-    const response = await Server.get("education");
-    return response;
-  },
+  getData: () => Server.get(),
+  getMainInfo: () => Server.get("mainInfo"),
+  getWorkExperience: () => Server.get("workExperience"),
+  getEducation: () => Server.get("education"),
 };
